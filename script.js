2d4b22b33d5e49def6609c6837ec1ee674387b50
Lisa Yepifanova <lisa.yepifanova@gmail.com>
2016-09-06 17:39:45 +0300

    remove script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function myFunction() {
-	if (document.getElementById("demo").class == "first_img") {
-   		document.getElementById("demo").src = "img/02.jpg";
-   		document.getElementById("demo").class = "second_img";
-	} else {
-		document.getElementById("demo").src = "img/01.jpg";
-		document.getElementById("demo").class = "first_img";
-	}
-}
